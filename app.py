29da141e5aaef37d40ef588ef1d7f4fa7ede82b4
giabaoyah271 <huevogiabao@gmail.com>
2026-04-21 20:05:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -115,12 +115,12 @@ def calculate_indicators(df):
             continue
         buy_score = 0.0
         sell_score = 0.0
-        max_score = 19.0 # Tổng điểm tuyệt đối
+        max_score = 18.0 # Tổng điểm tuyệt đối
         reason_list = [] # Lưu lý do của phiên hiện tại
         # Nếu giá vượt đỉnh 20 phiên với Volume lớn -> Tự động cộng điểm cực cao
         prev_max_20 = df['Price_Max_20'].iloc[i-1]
         if row['close'] >= prev_max_20 and row['volume'] > 1.8 * row['Vol_Avg']:
-            buy_score += 3.0 
+            buy_score += 2.5 
             reason_list.append("FAST TRACK: Bùng nổ Giá & Vol")
         if row['MFI'] > 55: 
             buy_score += 3.0; reason_list.append("Dòng tiền vào mạnh")
@@ -132,9 +132,9 @@ def calculate_indicators(df):
             sell_score += 2.0; reason_list.append("Giá < MA200")
         if row['volume'] > 1.8 * row['Vol_Avg']: 
             if row['close'] > row['open']: 
-                buy_score += 2.5; reason_list.append("Cầu mạnh (Vol đột biến)")
+                buy_score += 2.0; reason_list.append("Cầu mạnh (Vol đột biến)")
             elif row['close'] < row['open']:
-                sell_score += 2.5; reason_list.append("Bán tháo (Vol đột biến)")
+                sell_score += 2.0; reason_list.append("Bán tháo (Vol đột biến)")
             else:
                 pass
         # Nếu ADX > 25: Xu hướng mạnh, tin tưởng vào các chỉ báo hướng
